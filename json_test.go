6136b95de1ed58f61107ff20778d548a92c0af67
tongyulong <tongyulong@bytedance.com>
2020-12-06 21:41:26 +0800

    init

---
+++ json_test.go
@@ -0,0 +1,19 @@
+package study
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestA(t *testing.T) {
+	fmt.Println(len("git"))
+}
+
+func TestB(t *testing.T) {
+	fmt.Println("TestB is tested.")
+}
+
+func TestMain(m *testing.M) {
+	fmt.Println("TestMain is called.")
+	m.Run()
+}
